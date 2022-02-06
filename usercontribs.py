f2c3abdd2ef724c2a553fd0ca600996d2d40d043
Antti Kuparinen <akuparinen@iki.fi>
2022-02-06 21:33:45 +0200

    Refactoring

--- usercontribs.py
+++ usercontribs.py
@@ -3,62 +3,115 @@
 
 import sys
 import time
-import apina
+import mwapi
 import argparse
 import json
+import re
 
-from pprint import pprint
+def print_err(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
+def fix_endpoint(endpoint):
+    if endpoint.startswith("http://"):
+        endpoint = endpoint.replace("http://", "https://")
+        
+    if not endpoint.startswith("https://"):
+        endpoint = "https://" + endpoint
+    
+    if not endpoint.endswith("/w/api.php"):
+        endpoint = endpoint + "/w/api.php"
 
-parser = argparse.ArgumentParser(description='Optional app description')
+    return endpoint
 
-parser.add_argument('-o', '--output', type=str,
-                    help='Tulostettavan tiedoston nimi.')
-
-parser.add_argument('-u', '--user', type=str,
-                    help='Haettava käyttäjä')
-
-parser.add_argument('-a', '--api-url', type=str,
-                    help='APIn URL')
-
-args = parser.parse_args()
-
-filename = args.output
-user = args.user
-apiurl = args.api_url or "https://fi.wiktionary.org/w/api.php"
-
-print("API:", apiurl)
-if __name__ == "__main__":
-    api = apina.Apina(apiurl)
-
+def get(user, startdate, enddate, output, api, props):
+    if not props or props == []:
+        props = ["title", "timestamp", "comment"]
+        
     query = { 
         'action'        : 'query', 
         'list'          : 'usercontribs', 
         'ucuser'        : user,
-        'ucprop'        : 'title|comment',
+        'ucprop'        : "|".join(props),
         'uclimit'       : 500,
+        'ucstart'       : startdate,
+        'ucend'         : enddate,
         'uccontinue'    : None,
     }
 
-    if not filename:
-        print(f'Käyttö: {sys.argv[0]} --output <tiedosto> --user <käyttäjä> --api-url <apin url>')
+    query_handler = api.general_list_query(query, 'uccontinue')
+    page_list = []
+    continued = False
+    for partial_list in query_handler.next():
+        page_list = page_list + partial_list
+        if query_handler.continues():
+            sys.stderr.write('.')
+            time.sleep(5)
+            continued = True
+
+    if continued:
+        sys.stderr.write('\n')
+        
+    for line in page_list:
+        for prop in props:
+            output.write(line[prop])
+            if prop != props[-1]:
+                output.write(';')
+                
+        output.write('\n');
+
+    print_err(f"Wrote {len(page_list)}")
+        
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Optional app description')
+
+    parser.add_argument('-o', '--output', type=str,
+                        help='Output file, or - for stdout')
+    
+    parser.add_argument('-u', '--user', type=str,
+                        help='User name (without User:)')
+    
+    parser.add_argument('-e', '--endpoint', type=str,
+                        help='Endpoint URL')
+
+    parser.add_argument('-f', '--from', type=str, dest="from_",
+                        help='Start time in ISO-8601 format (YYYY-MM-DDTHH:mm:ss)')
+    
+    parser.add_argument('-t', '--to', type=str,
+                        help='End time in ISO-8601 format')
+
+    parser.add_argument('-p', '--props', type=str,
+                        help='Props to return (title, timestamp, comment)')
+    
+    try:
+        args = parser.parse_args()
+    except:
         exit(1)
+    
+    filename = args.output
+    user = args.user
+    endpoint = args.endpoint
+    startdate = args.from_
+    enddate = args.to
+    props = args.props
 
-    with open(filename, 'a') as the_file:
-        for list_ in api.general_query(query, "uccontinue"):
-            for line in list_:
-                the_file.write(line['title'])
-                the_file.write(';')
-                #the_file.write(line['timestamp'])
-                #the_file.write(';')
-                the_file.write(line['comment'])
-                the_file.write('\n')
+    if props:
+        props = re.split(r"[^a-z]", props)
 
-            eka = list_[0]
-            vika = list_[len(list_)-1]
-            print(f"Kirjoitettiin {len(list_)} riviä ('{eka}' – '{vika}'")
+    endpoint_fixed = fix_endpoint(endpoint)
+    if endpoint_fixed != endpoint:
+        endpoint = endpoint_fixed
+        print_err(f"Endpoint: {endpoint}")
+    
+    api = mwapi.MWApi(endpoint)
 
-            
+    if filename or filename == '-':
+        output = sys.stdout
+    else:
+        output = open(filename, 'a')
 
+
+    get(user, startdate, enddate, output, api, props)
 
         
